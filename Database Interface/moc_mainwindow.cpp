f3170c21a93b92bd238faa162120ea679271748a
Xavier López Asensio <163464614+XavierLoAs@users.noreply.github.com>
2024-05-10 01:10:43 +0200

    Add files via upload

---
+++ Database Interface/moc_mainwindow.cpp
@@ -0,0 +1,305 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.5.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../../mainwindow.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.5.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
+    "MainWindow",
+    "on_actionSalir_del_programa_triggered",
+    "",
+    "on_actionAcerca_de_QT_triggered",
+    "on_actionAcceder_a_Microsoft_SQL_Server_2020_triggered",
+    "on_pushButton_clicked",
+    "on_pushButton_2_clicked",
+    "on_pushButton_3_clicked",
+    "on_pushButton_4_clicked",
+    "on_pushButton_5_clicked",
+    "on_pushButton_6_clicked",
+    "on_radioButton_clicked",
+    "on_radioButton_2_clicked",
+    "on_pushButton_7_clicked",
+    "on_actionCambiar_tema_triggered",
+    "on_lineEdit_4_returnPressed",
+    "executeQuery",
+    "statement",
+    "on_pushButton_10_clicked",
+    "on_pushButton_8_clicked",
+    "on_pushButton_9_clicked"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
+    uint offsetsAndSizes[42];
+    char stringdata0[11];
+    char stringdata1[38];
+    char stringdata2[1];
+    char stringdata3[32];
+    char stringdata4[55];
+    char stringdata5[22];
+    char stringdata6[24];
+    char stringdata7[24];
+    char stringdata8[24];
+    char stringdata9[24];
+    char stringdata10[24];
+    char stringdata11[23];
+    char stringdata12[25];
+    char stringdata13[24];
+    char stringdata14[32];
+    char stringdata15[28];
+    char stringdata16[13];
+    char stringdata17[10];
+    char stringdata18[25];
+    char stringdata19[24];
+    char stringdata20[24];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_stringdata_CLASSMainWindowENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 10),  // "MainWindow"
+        QT_MOC_LITERAL(11, 37),  // "on_actionSalir_del_programa_t..."
+        QT_MOC_LITERAL(49, 0),  // ""
+        QT_MOC_LITERAL(50, 31),  // "on_actionAcerca_de_QT_triggered"
+        QT_MOC_LITERAL(82, 54),  // "on_actionAcceder_a_Microsoft_..."
+        QT_MOC_LITERAL(137, 21),  // "on_pushButton_clicked"
+        QT_MOC_LITERAL(159, 23),  // "on_pushButton_2_clicked"
+        QT_MOC_LITERAL(183, 23),  // "on_pushButton_3_clicked"
+        QT_MOC_LITERAL(207, 23),  // "on_pushButton_4_clicked"
+        QT_MOC_LITERAL(231, 23),  // "on_pushButton_5_clicked"
+        QT_MOC_LITERAL(255, 23),  // "on_pushButton_6_clicked"
+        QT_MOC_LITERAL(279, 22),  // "on_radioButton_clicked"
+        QT_MOC_LITERAL(302, 24),  // "on_radioButton_2_clicked"
+        QT_MOC_LITERAL(327, 23),  // "on_pushButton_7_clicked"
+        QT_MOC_LITERAL(351, 31),  // "on_actionCambiar_tema_triggered"
+        QT_MOC_LITERAL(383, 27),  // "on_lineEdit_4_returnPressed"
+        QT_MOC_LITERAL(411, 12),  // "executeQuery"
+        QT_MOC_LITERAL(424, 9),  // "statement"
+        QT_MOC_LITERAL(434, 24),  // "on_pushButton_10_clicked"
+        QT_MOC_LITERAL(459, 23),  // "on_pushButton_8_clicked"
+        QT_MOC_LITERAL(483, 23)   // "on_pushButton_9_clicked"
+    },
+    "MainWindow",
+    "on_actionSalir_del_programa_triggered",
+    "",
+    "on_actionAcerca_de_QT_triggered",
+    "on_actionAcceder_a_Microsoft_SQL_Server_2020_triggered",
+    "on_pushButton_clicked",
+    "on_pushButton_2_clicked",
+    "on_pushButton_3_clicked",
+    "on_pushButton_4_clicked",
+    "on_pushButton_5_clicked",
+    "on_pushButton_6_clicked",
+    "on_radioButton_clicked",
+    "on_radioButton_2_clicked",
+    "on_pushButton_7_clicked",
+    "on_actionCambiar_tema_triggered",
+    "on_lineEdit_4_returnPressed",
+    "executeQuery",
+    "statement",
+    "on_pushButton_10_clicked",
+    "on_pushButton_8_clicked",
+    "on_pushButton_9_clicked"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
+
+ // content:
+      11,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      18,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,  122,    2, 0x08,    1 /* Private */,
+       3,    0,  123,    2, 0x08,    2 /* Private */,
+       4,    0,  124,    2, 0x08,    3 /* Private */,
+       5,    0,  125,    2, 0x08,    4 /* Private */,
+       6,    0,  126,    2, 0x08,    5 /* Private */,
+       7,    0,  127,    2, 0x08,    6 /* Private */,
+       8,    0,  128,    2, 0x08,    7 /* Private */,
+       9,    0,  129,    2, 0x08,    8 /* Private */,
+      10,    0,  130,    2, 0x08,    9 /* Private */,
+      11,    0,  131,    2, 0x08,   10 /* Private */,
+      12,    0,  132,    2, 0x08,   11 /* Private */,
+      13,    0,  133,    2, 0x08,   12 /* Private */,
+      14,    0,  134,    2, 0x08,   13 /* Private */,
+      15,    0,  135,    2, 0x08,   14 /* Private */,
+      16,    1,  136,    2, 0x08,   15 /* Private */,
+      18,    0,  139,    2, 0x08,   17 /* Private */,
+      19,    0,  140,    2, 0x08,   18 /* Private */,
+      20,    0,  141,    2, 0x08,   19 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   17,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSMainWindowENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSMainWindowENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'on_actionSalir_del_programa_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_actionAcerca_de_QT_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_actionAcceder_a_Microsoft_SQL_Server_2020_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_3_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_4_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_5_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_6_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_radioButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_radioButton_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_7_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_actionCambiar_tema_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_lineEdit_4_returnPressed'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'executeQuery'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QString, std::false_type>,
+        // method 'on_pushButton_10_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_8_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_9_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_actionSalir_del_programa_triggered(); break;
+        case 1: _t->on_actionAcerca_de_QT_triggered(); break;
+        case 2: _t->on_actionAcceder_a_Microsoft_SQL_Server_2020_triggered(); break;
+        case 3: _t->on_pushButton_clicked(); break;
+        case 4: _t->on_pushButton_2_clicked(); break;
+        case 5: _t->on_pushButton_3_clicked(); break;
+        case 6: _t->on_pushButton_4_clicked(); break;
+        case 7: _t->on_pushButton_5_clicked(); break;
+        case 8: _t->on_pushButton_6_clicked(); break;
+        case 9: _t->on_radioButton_clicked(); break;
+        case 10: _t->on_radioButton_2_clicked(); break;
+        case 11: _t->on_pushButton_7_clicked(); break;
+        case 12: _t->on_actionCambiar_tema_triggered(); break;
+        case 13: _t->on_lineEdit_4_returnPressed(); break;
+        case 14: _t->executeQuery((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        case 15: _t->on_pushButton_10_clicked(); break;
+        case 16: _t->on_pushButton_8_clicked(); break;
+        case 17: _t->on_pushButton_9_clicked(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSMainWindowENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 18)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 18;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 18)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 18;
+    }
+    return _id;
+}
+QT_WARNING_POP
